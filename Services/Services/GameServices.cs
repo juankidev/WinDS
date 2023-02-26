3da8b9a377b3cfd15f76f4563299532421f4c40d
juankidev <juancamdev@gmail.com>
2023-02-26 02:45:21 -0500

    Agregar archivos de proyecto.

---
+++ Services/Services/GameServices.cs
@@ -0,0 +1,79 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Services.Services
+{
+    public class GameServices
+    {
+        private readonly List<string> _responseList;
+
+        public GameServices()
+        {
+            _responseList = GetDictionary();
+        }
+
+        public List<string> GetDictionary()
+        {
+            string rutaArchivo = "dictionary.txt";
+
+            List<string> lineasArchivo = new List<string>();
+
+            using (StreamReader sr = new StreamReader(rutaArchivo))
+            {
+                string linea;
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    lineasArchivo.Add(linea);
+                }
+            }
+
+            return lineasArchivo;
+        }
+
+        public List<string> GetCombinations(string inputLetters, int inputLength)
+        {
+            List<string> validWords = new List<string>();
+            List<char> chars = new List<char>(inputLetters.ToCharArray());
+
+            foreach (string word in _responseList)
+            {
+                // Clonar la lista de caracteres para que no se modifique la original
+                List<char> remainingChars = new List<char>(chars);
+
+                bool isValid = true;
+
+                // Iterar sobre cada letra en la palabra
+                foreach (char letter in word)
+                {
+                    // Buscar la letra en la lista de caracteres
+                    int index = remainingChars.IndexOf(letter);
+
+                    // Si la letra no se encuentra en la lista, la palabra no es válida
+                    if (index == -1)
+                    {
+                        isValid = false;
+                        break;
+                    }
+
+                    // Si la letra se encuentra en la lista, quitarla de la lista para no utilizarla de nuevo
+                    remainingChars.RemoveAt(index);
+                }
+
+                // Si la palabra es válida, añadirla a la lista de palabras válidas
+                if (isValid)
+                {
+                    validWords.Add(word);
+                }
+            }
+
+            validWords = validWords.FindAll(x => x.Length == inputLength);
+            return validWords;
+        }
+    }
+}
---
+++ WinDS/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+
+namespace WinDS.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly ILogger<GameController> _logger;
+        private readonly GameServices _gameService;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+            _gameService = new GameServices();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CombinationsPartial(string inputLetters, int inputLength)
+        {
+            ViewBag.Combinations = _gameService.GetCombinations(inputLetters.ToLower(), inputLength);
+            return PartialView("Combinations");
+        }
+
+    }
+}
